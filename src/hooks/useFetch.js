fcfe248cbce31a14fe62fbfb896224c482e23fbb
Rob <robert.chamberlain@cmeducations.se>
2026-03-24 16:26:08 +0100

    Endpoint

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -2,13 +2,13 @@ import {useState, useEffect} from 'react'
 
  const useFetch = endpoint => {
     const [data, setData] = useState(null)
-    console.log("ww", import.meta.env.VITE_API_ENDPOINT)
+    console.log("ww", import.meta.env.API_ENDPOINT)
 
-    const API_ENDPOINT =`${import.meta.env.VITE_API_ENDPOINT}${endpoint}`
+    const APIENDPOINT =`${import.meta.env.API_ENDPOINT}${endpoint}`
 
     const getData = async () => {
         try {
-            let response = await fetch(API_ENDPOINT)
+            let response = await fetch(APIENDPOINT)
             let data = await response.json()
             setData(data.data)
         } catch (error) {
